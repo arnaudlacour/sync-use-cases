2d2d86cf0787116016c847ea1e29995203bbf4ee
arno <arnaudlacour@pingidentity.com>
2020-04-16 23:09:10 -0400

    update with detailed output

---
+++ persistent-search-source/README.md
@@ -0,0 +1,10 @@
+# Persistent Search PingDataSync Source
+
+## TLDR;
+https://pingidentity-devops.gitbook.io/devops/getstarted
+then
+```
+git clone https://github.com/arnaudlacour/sync-use-cases.git
+docker-compose up -d
+docker-compose logs -f
+```
--- persistent-search-source/pingdatasync/hooks/80-post-start.sh
+++ persistent-search-source/pingdatasync/hooks/80-post-start.sh
@@ -4,4 +4,4 @@ ${VERBOSE} && set -x
 # shellcheck source=/dev/null
 test -f "${HOOKS_DIR}/pingcommon.lib.sh" && . "${HOOKS_DIR}/pingcommon.lib.sh"
 
-wait-for localhost:${LDAP_PORT} -t 900 -- modrate --hostname pingdirectory --port ${LDAP_PORT} --entryDN "cn=user.[0-9],ou=People,dc=example,dc=com" --atttribute description --ratePerSecond 1
+wait-for localhost:${LDAP_PORT} -t 900 -- modrate --hostname pingdirectory --port ${LDAP_PORT}  --atttribute description --ratePerSecond 1 --entryDN "cn=user.[0-9],ou=People,dc=example,dc=com"
