3cf077241d0e2b2719efaeb477530652aa7d1479
arno <arnaudlacour@pingidentity.com>
2020-04-16 22:29:09 -0400

    fix initial mistakes

--- persistent-search-source/pingdatasync/hooks/80-post-start.sh
+++ persistent-search-source/pingdatasync/hooks/80-post-start.sh
@@ -4,15 +4,5 @@ ${VERBOSE} && set -x
 # shellcheck source=/dev/null
 test -f "${HOOKS_DIR}/pingcommon.lib.sh" && . "${HOOKS_DIR}/pingcommon.lib.sh"
 
-wait-for pingdirectory:${LDAP_PORT}
-wait-for localhost:${LDAP_PORT}
-
-# waaaaaaaaitaminit
-sleep 60
-
-modrate \
-    --hostname pingdirectory \
-    --port ${LDAP_PORT} \
-    --entryDN "cn=user.[0-9],ou=People,dc=example,dc=com" \
-    --atttribute description \
-    --ratePerSecond 1
+wait-for localhost:${LDAP_PORT} -t 900 -- sleep 60
+wait-for pingdirectory:${LDAP_PORT} -t 900 -- modrate --hostname pingdirectory --port ${LDAP_PORT} --entryDN "cn=user.[0-9],ou=People,dc=example,dc=com" --atttribute description --ratePerSecond 1
