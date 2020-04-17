e29636b0bb56eb28b1355f57c5f6e94fd1c4c350
arno <arnaudlacour@pingidentity.com>
2020-04-16 22:38:35 -0400

    wait for pingdatasync to be available

--- persistent-search-source/pingdatasync/hooks/80-post-start.sh
+++ persistent-search-source/pingdatasync/hooks/80-post-start.sh
@@ -4,5 +4,4 @@ ${VERBOSE} && set -x
 # shellcheck source=/dev/null
 test -f "${HOOKS_DIR}/pingcommon.lib.sh" && . "${HOOKS_DIR}/pingcommon.lib.sh"
 
-wait-for localhost:${LDAP_PORT} -t 900 -- sleep 60
-wait-for pingdirectory:${LDAP_PORT} -t 900 -- modrate --hostname pingdirectory --port ${LDAP_PORT} --entryDN "cn=user.[0-9],ou=People,dc=example,dc=com" --atttribute description --ratePerSecond 1
+wait-for localhost:${LDAP_PORT} -t 900 -- modrate --hostname pingdirectory --port ${LDAP_PORT} --entryDN "cn=user.[0-9],ou=People,dc=example,dc=com" --atttribute description --ratePerSecond 1
